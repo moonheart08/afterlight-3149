518a9bf68bafa36dab8abc30a94ab20b03ce68f7
moonheart08 <moonheart08@users.noreply.github.com>
2022-10-28 20:43:17 -0500

    fixe

--- Content.Client/UserInterface/Systems/Ghost/GhostUIController.cs
+++ Content.Client/UserInterface/Systems/Ghost/GhostUIController.cs
@@ -1,9 +1,13 @@
 ﻿using Content.Client.Gameplay;
 using Content.Client.Ghost;
 using Content.Client.UserInterface.Systems.Ghost.Widgets;
+using Content.Shared.CCVar;
 using Content.Shared.Ghost;
 using Robust.Client.UserInterface;
 using Robust.Client.UserInterface.Controllers;
+using Robust.Shared.Configuration;
+using Robust.Shared.Console;
+using Robust.Shared.Timing;
 
 namespace Content.Client.UserInterface.Systems.Ghost;
 
@@ -11,6 +15,9 @@ namespace Content.Client.UserInterface.Systems.Ghost;
 public sealed class GhostUIController : UIController, IOnSystemChanged<GhostSystem>
 {
     [Dependency] private readonly IEntityNetworkManager _net = default!;
+    [Dependency] private readonly IGameTiming _gameTiming = default!;
+    [Dependency] private readonly IConsoleHost _consoleHost = default!;
+    [Dependency] private readonly IConfigurationManager _cfg = default!;
 
     [UISystemDependency] private readonly GhostSystem? _system = default;
 
@@ -44,7 +51,9 @@ public sealed class GhostUIController : UIController, IOnSystemChanged<GhostSyst
         }
 
         Gui.Visible = _system?.IsGhost ?? false;
-        Gui.Update(_system?.AvailableGhostRoleCount, _system?.Player?.CanReturnToBody);
+        Gui.Update(_system?.AvailableGhostRoleCount, _system?.Player?.CanReturnToBody,
+            _cfg.GetCVar(CCVars.AllowRespawns) ? _system?.Player?.TimeOfDeath : null,
+            _cfg.GetCVar(CCVars.RespawnTime));
     }
 
     private void OnPlayerRemoved(GhostComponent component)
@@ -100,8 +109,14 @@ public sealed class GhostUIController : UIController, IOnSystemChanged<GhostSyst
         Gui.ReturnToBodyPressed += ReturnToBody;
         Gui.GhostRolesPressed += GhostRolesPressed;
         Gui.TargetWindow.WarpClicked += OnWarpClicked;
+        Gui.GhostRolesRespawnPressed += GuiOnGhostRolesRespawnPressed;
 
         UpdateGui();
+    }
+
+    private void GuiOnGhostRolesRespawnPressed()
+    {
+        _consoleHost.ExecuteCommand("ghostrespawn");
     }
 
     public void UnloadGui()
@@ -113,6 +128,7 @@ public sealed class GhostUIController : UIController, IOnSystemChanged<GhostSyst
         Gui.ReturnToBodyPressed -= ReturnToBody;
         Gui.GhostRolesPressed -= GhostRolesPressed;
         Gui.TargetWindow.WarpClicked -= OnWarpClicked;
+        Gui.GhostRolesRespawnPressed -= GuiOnGhostRolesRespawnPressed;
 
         Gui.Hide();
     }
